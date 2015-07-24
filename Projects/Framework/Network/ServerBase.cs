de60cecbd23e793a3a54fe93d4ddb819cf0ca90c
Fabian <fabian@multi-emu.com>
2015-07-24 11:49:27 +0200

    Added ORM library as submodule. Added ServerBase class.

---
+++ Projects/Framework/Network/ServerBase.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Multi-Emu.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Framework.Network
+{
+    public class ServerBase : IDisposable
+    {
+        TcpListener listener;
+        bool isRunning;
+
+        public ServerBase(string ip, int port)
+        {
+            var bindIP = IPAddress.None;
+
+            try
+            {
+                listener = new TcpListener(bindIP, port);
+                listener.Start();
+
+                if (isRunning = listener.Server.IsBound)
+                    new Thread(AcceptConnection).Start(200);
+            }
+            catch
+            {
+            }
+        }
+
+        async void AcceptConnection(object delay)
+        {
+            while (isRunning)
+            {
+                Thread.Sleep((int)delay);
+
+                if (listener.Pending())
+                {
+                    var clientSocket = await listener.AcceptSocketAsync();
+
+                    if (clientSocket != null)
+                        await DoWork(clientSocket);
+                }
+            }
+        }
+
+        public virtual Task DoWork(Socket client)
+        {
+            return null;
+        }
+
+        public void Dispose()
+        {
+            listener = null;
+            isRunning = false;
+        }
+    }
+}
