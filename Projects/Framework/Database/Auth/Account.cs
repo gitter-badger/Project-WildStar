9f9de125ceabc7b98d58682075bb69ced44678b5
Fabian <fabian@multi-emu.com>
2015-07-30 06:59:27 +0200

    Added DB class.

---
+++ Projects/Framework/Database/Auth/Account.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Multi-Emu.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Lappa_ORM;
+
+namespace Framework.Database.Auth
+{
+    public class Account : Entity
+    {
+        public uint Id                 { get; set; }
+        public string Email            { get; set; }
+        public string UserName         { get; set; }
+        public string FirstName        { get; set; }
+        public string LastName         { get; set; }
+        public string PasswordVerifier { get; set; }
+        public string Salt             { get; set; }
+        public bool Online             { get; set; }
+    }
+}
---
+++ Projects/Framework/Database/DB.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Multi-Emu.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Lappa_ORM;
+
+namespace Framework.Database
+{
+    public class DB
+    {
+        public static Lappa_ORM.Database Auth = new Lappa_ORM.Database();
+
+        public static string CreateConnectionString(string host, string user, string password, string database, int port, int minPoolSize, int maxPoolSize, ConnectionType connType)
+        {
+            if (connType == ConnectionType.MYSQL)
+                return $"Server={host};User Id={user};Port={port};Password={password};Database={database};Allow Zero Datetime=True;Pooling=True;Min Pool Size={minPoolSize};Max Pool Size={maxPoolSize};CharSet=utf8";
+
+            if (connType == ConnectionType.MSSQL)
+                return $"Data Source={host}; Initial Catalog = {database}; User ID = {user}; Password = {password};Pooling=True;Min Pool Size={minPoolSize};Max Pool Size={maxPoolSize}";
+
+            return null;
+        }
+    }
+}
