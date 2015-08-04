1b9d97e9497a8a2760ec2a4a60218d2cb8cce431
Fabian <fabian@multi-emu.com>
2015-08-04 04:15:41 +0200

    Added default AuthServer folder/file structure.

---
+++ Projects/AuthServer/Network/Packets/PacketManager.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Multi-Emu.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Network.Packets
+{
+    class PacketManager
+    {
+    }
+}
